b9054666ba0688309e5034647e89ae307438aee0
Domenico Teodonio <dteod@protonmail.com>
2022-12-30 17:31:37 +0100

    Removed public header file dependency on CMake's target mechanism

--- include/cco/arch/x86.h
+++ include/cco/arch/x86.h
@@ -25,29 +25,12 @@
  */
 typedef unsigned int cco_x86_settings;
 
-#if CCO_x86_ENABLE_EFLAGS_REGISTER_EXCHANGE
-#  define CCO_SETTINGS_x86_EXCHANGE_EFLAGS_REGISTER ((cco_x86_settings)(1 << 0))
-#endif
-
-#if CCO_x86_ENABLE_FPU_MMX_REGISTERS_EXCHANGE
-#  define CCO_SETTINGS_x86_EXCHANGE_FPU_MMX_REGISTERS ((cco_x86_settings)(1 << 1))
-#endif
-
-#if CCO_x86_ENABLE_SSE_REGISTERS_EXCHANGE
-#  define CCO_SETTINGS_x86_EXCHANGE_SSE_REGISTERS ((cco_x86_settings)(1 << 2))
-#endif
-
-#if CCO_x86_ENABLE_SEGMENT_REGISTERS_EXCHANGE
-#  define CCO_SETTINGS_x86_EXCHANGE_SEGMENT_REGISTERS ((cco_x86_settings)(1 << 3))
-#endif
-
-#if CCO_x86_ENABLE_DEBUG_REGISTERS_EXCHANGE
-#  define CCO_SETTINGS_x86_EXCHANGE_DEBUG_REGISTERS ((cco_x86_settings)(1 << 4))
-#endif
-
-#if CCO_x86_ENABLE_CONTROL_REGISTERS_EXCHANGE
-#  define CCO_SETTINGS_x86_EXCHANGE_CONTROL_REGISTERS ((cco_x86_settings)(1 << 5))
-#endif
+#define CCO_SETTINGS_x86_EXCHANGE_EFLAGS_REGISTER   ((cco_x86_settings)(1 << 0))
+#define CCO_SETTINGS_x86_EXCHANGE_FPU_MMX_REGISTERS ((cco_x86_settings)(1 << 1))
+#define CCO_SETTINGS_x86_EXCHANGE_SSE_REGISTERS     ((cco_x86_settings)(1 << 2))
+#define CCO_SETTINGS_x86_EXCHANGE_SEGMENT_REGISTERS ((cco_x86_settings)(1 << 3))
+#define CCO_SETTINGS_x86_EXCHANGE_DEBUG_REGISTERS   ((cco_x86_settings)(1 << 4))
+#define CCO_SETTINGS_x86_EXCHANGE_CONTROL_REGISTERS ((cco_x86_settings)(1 << 5))
 
 typedef cco_x86_settings cco_architecture_specific_settings;
 
