b023ae663b9c0f95da2e745369a9e7ecaaa40c96
Domenico Teodonio <dteod@protonmail.com>
2023-01-02 20:42:40 +0100

    Added x86_64 cmake support stub

--- cmake/toolchains/x86_64-Linux-gcc.cmake
+++ cmake/toolchains/x86_64-Linux-gcc.cmake
@@ -14,10 +14,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-
 if(NOT CCO_TOOLCHAIN_INCLUDED)
     set(CCO_TOOLCHAIN_INCLUDED TRUE)
     include(${CMAKE_CURRENT_LIST_DIR}/../DefaultSetting.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/../arch/x86_64.cmake)
+
+    set(CMAKE_SYSTEM_NAME Linux)
+    set(CMAKE_C_COMPILER gcc)
+    set(CMAKE_CXX_COMPILER g++)
+
+    target_compile_definitions(cco_arch_${CMAKE_SYSTEM_PROCESSOR} INTERFACE -DCCO_x86_64_CALLING_CONVENTION=CCO_x86_64_CALL_SYSV)
+    target_compile_definitions(cco_arch_${CMAKE_SYSTEM_PROCESSOR} INTERFACE -DCCO_x86_64_CALLING_CONVENTION=CCO_x86_64_CALL_x64)
 endif()
