26d4b545a4c864bae0fde75f64067062ba9c6033
Domenico Teodonio <dteod@protonmail.com>
2023-01-02 20:43:54 +0100

    Added more tests to the black box test suite

--- test/black_box.cpp
+++ test/black_box.cpp
@@ -30,6 +30,11 @@
  */
 
 #include <cco.h>
+
+#include <chrono>
+#include <random>
+#include <thread>
+
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
@@ -72,9 +77,7 @@ TEST_CASE("Test 4: Resume a coroutine after yielding control", "[cco]")
     cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
     REQUIRE(coroutine != NULL);
 
-    int  argument = 0;
-    int* ret      = nullptr;
-
+    int argument = 0;
     REQUIRE(cco_coroutine_start(
         coroutine,
         [](void* argument) -> void {
@@ -83,7 +86,6 @@ TEST_CASE("Test 4: Resume a coroutine after yielding control", "[cco]")
             *value = 1;
             cco_suspend();
             *value = 2;
-            cco_return(value);
         },
         &argument
     ));
@@ -98,8 +100,321 @@ TEST_CASE("Test 4: Resume a coroutine after yielding control", "[cco]")
     REQUIRE(argument == 2);
     REQUIRE(cco_coroutine_get_state(coroutine) == CCO_COROUTINE_STATE_UNSCHEDULED);
 
-    ret = reinterpret_cast<int*>(cco_coroutine_get_return_value(coroutine));
-    REQUIRE(&argument == ret);
+    cco_coroutine_destroy(coroutine);
+}
 
+TEST_CASE("Test 5: Return a value from the coroutine stack", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    REQUIRE(coroutine != NULL);
+
+    int* ret = nullptr;
+    REQUIRE(cco_coroutine_start(
+        coroutine,
+        [](void*) -> void {
+            int value = 42;
+            cco_return(&value);
+        },
+        NULL
+    ));
+    REQUIRE(cco_coroutine_get_state(coroutine) == CCO_COROUTINE_STATE_UNSCHEDULED);
+    ret = reinterpret_cast<int*>(cco_coroutine_get_return_value(coroutine));
+    REQUIRE(*ret == 42);
+
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 6: Yield values from a coroutine", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    REQUIRE(coroutine != NULL);
+
+    int argument = 4;
+    REQUIRE(cco_coroutine_start(
+        coroutine,
+        [](void* argument) -> void {
+            int* arg   = reinterpret_cast<int*>(argument);
+            int  value = 10 + *arg;
+            cco_yield(&value);
+
+            double value2 = 3.14 + *arg * 2;
+            cco_yield(&value2);
+        },
+        &argument
+    ));
+    REQUIRE(cco_coroutine_get_state(coroutine) == CCO_COROUTINE_STATE_SUSPENDED);
+    {
+        int* yielded = reinterpret_cast<int*>(cco_coroutine_get_return_value(coroutine));
+        REQUIRE(*yielded == 14);
+    }
+    argument = 5;
+    cco_resume(coroutine);
+    {
+        double* yielded = reinterpret_cast<double*>(cco_coroutine_get_return_value(coroutine));
+        REQUIRE(*yielded == 3.14 + 5 * 2);
+    }
+    REQUIRE(cco_coroutine_get_state(coroutine) == CCO_COROUTINE_STATE_SUSPENDED);
+    cco_resume(coroutine);
+    REQUIRE(cco_coroutine_get_state(coroutine) == CCO_COROUTINE_STATE_UNSCHEDULED);
+    REQUIRE(cco_coroutine_get_return_value(coroutine) == nullptr);
+
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 7: Error when trying to resume an invalid coroutine", "[cco]")
+{
+    cco_resume(nullptr);
+    REQUIRE(cco_errno == CCO_ERROR_INVALID_ARGUMENT);
+}
+
+TEST_CASE("Test 8: Error when trying to resume a coroutine that is not suspended", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    REQUIRE(coroutine != NULL);
+    cco_resume(coroutine);
+    REQUIRE(cco_errno == CCO_ERROR_NOT_SUSPENDED);
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 9: Error when trying to resume a coroutine that is already running", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    cco_error      err       = CCO_OK;
+    REQUIRE(coroutine != NULL);
+    REQUIRE(cco_coroutine_start(
+        coroutine,
+        [](void* err) {
+            cco_resume(cco_this_coroutine());
+            *reinterpret_cast<cco_error*>(err) = cco_errno;
+        },
+        &err
+    ));
+    REQUIRE(cco_errno == CCO_OK);
+    REQUIRE(err == CCO_ERROR_NOT_SUSPENDED);
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 10: Error when trying to resume a coroutine that is already finished", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    REQUIRE(coroutine != NULL);
+    REQUIRE(cco_coroutine_start(
+        coroutine,
+        [](void*) {
+            int value = 42;
+            cco_return(&value);
+        },
+        NULL
+    ));
+    REQUIRE(cco_coroutine_get_state(coroutine) == CCO_COROUTINE_STATE_UNSCHEDULED);
+    cco_resume(coroutine);
+    REQUIRE(cco_errno == CCO_ERROR_NOT_SUSPENDED);
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 11: Error when trying to resume the main context from a coroutine", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    cco_error      err       = CCO_OK;
+    REQUIRE(coroutine != NULL);
+
+    struct Args {
+        cco_coroutine* main;
+        cco_error*     err;
+    } args = {cco_this_coroutine(), &err};
+
+    REQUIRE(cco_coroutine_start(
+        coroutine,
+        [](void* arg) {
+            Args* args = reinterpret_cast<Args*>(arg);
+            cco_resume(args->main);
+            *reinterpret_cast<cco_error*>(args->err) = cco_errno;
+        },
+        &args
+    ));
+    REQUIRE(err == CCO_ERROR_INVALID_ARGUMENT);
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 12: cco_this_coroutine() in the main context returns NULL", "[cco]")
+{
+    REQUIRE(cco_this_coroutine() == NULL);
+}
+
+TEST_CASE("Test 13: cco_this_coroutine() in a coroutine returns the coroutine", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    REQUIRE(coroutine != NULL);
+    REQUIRE(cco_coroutine_start(
+        coroutine, [](void* coroutine) { REQUIRE(cco_this_coroutine() == coroutine); }, coroutine
+    ));
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 14: Error in retrieving main context's execution state", "[cco]")
+{
+    REQUIRE(cco_coroutine_get_state(cco_this_coroutine()) == CCO_COROUTINE_STATE_NONE);
+    REQUIRE(cco_errno == CCO_ERROR_INVALID_ARGUMENT);
+}
+
+TEST_CASE("Test 15: Error in retrieving main context's return value", "[cco]")
+{
+    REQUIRE(cco_coroutine_get_return_value(cco_this_coroutine()) == NULL);
+    REQUIRE(cco_errno == CCO_ERROR_INVALID_ARGUMENT);
+}
+
+TEST_CASE("Test 16: Error in retrieving main context's stack size", "[cco]")
+{
+    REQUIRE(cco_coroutine_get_stack_size(cco_this_coroutine()) == 0);
+    REQUIRE(cco_errno == CCO_ERROR_INVALID_ARGUMENT);
+}
+
+TEST_CASE("Test 17: Retrieving coroutine's stack size", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    REQUIRE(coroutine != NULL);
+    REQUIRE(cco_coroutine_get_stack_size(coroutine) == CCO_DEFAULT_STACK_SIZE);
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 18: Retrieving coroutine's stack usage", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    REQUIRE(coroutine != NULL);
+    cco_coroutine_start(
+        coroutine,
+        [](void*) {
+            volatile int32_t                       v = 0;
+            volatile int32_t                       values[32];
+            std::mt19937                           engine;
+            std::uniform_int_distribution<int32_t> dist;
+            for(int i = 0; i != sizeof(values) / sizeof(int32_t); ++i) {
+                values[i] = dist(engine);
+            }
+            for(int i = 0; i != sizeof(values) / sizeof(int32_t); ++i) {
+                v = v + values[i];
+            }
+            cco_suspend();
+        },
+        NULL
+    );
+
+    REQUIRE(
+        cco_coroutine_get_stack_usage(coroutine)
+        > (sizeof(int32_t) * 32 + sizeof(int32_t) + sizeof(std::mt19937) + sizeof(std::uniform_int_distribution<int32_t>))
+    );
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 19: Destroy a suspended coroutine", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    REQUIRE(coroutine != NULL);
+    cco_coroutine_start(
+        coroutine, [](void*) { cco_suspend(); }, NULL
+    );
+    cco_coroutine_destroy(coroutine);
+    REQUIRE(cco_errno == CCO_OK);
+}
+
+TEST_CASE("Test 20: Error destroying the current coroutine", "[cco]")
+{
+    cco_coroutine* coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    cco_error      err;
+    REQUIRE(coroutine != NULL);
+    REQUIRE(cco_coroutine_start(
+        coroutine,
+        [](void* err) {
+            cco_coroutine_destroy(cco_this_coroutine());
+            *reinterpret_cast<cco_error*>(err) = cco_errno;
+        },
+        &err
+    ));
+    REQUIRE(err == CCO_ERROR_INVALID_CONTEXT);
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 21: Awaiting a ready/synchronous operation does not suspend", "[cco]")
+{
+    cco_coroutine*              coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    std::vector<cco_coroutine*> coroutines;
+    int                         result = 0;
+
+    struct Args {
+        std::vector<cco_coroutine*>* coroutines;
+        int*                         result;
+    } args = {&coroutines, &result};
+
+    REQUIRE(coroutine != NULL);
+
+    cco_coroutine_start(
+        coroutine,
+        [](void* args_) {
+            cco_suspend();
+            Args* args = reinterpret_cast<Args*>(args_);
+            cco_await_with(
+                cco_await_ready,
+                [](cco_coroutine*, void* arg) {
+                    std::vector<cco_coroutine*>* coroutines = reinterpret_cast<std::vector<cco_coroutine*>*>(arg);
+                    coroutines->push_back(cco_this_coroutine());
+                    return true;
+                },
+                args->coroutines
+            );
+            *reinterpret_cast<int*>(args->result) = 1;
+        },
+        &args
+    );
+    REQUIRE(result == 0);
+    cco_resume(coroutine);
+
+    REQUIRE(coroutines.size() == 0);
+    // cco_resume(coroutines.back()) not needed. The coroutine ends immediately after the await operation without suspending.
+
+    REQUIRE(result == 1);
+    cco_coroutine_destroy(coroutine);
+}
+
+TEST_CASE("Test 22: Awaiting for 3 seconds on a result", "[cco]")
+{
+    cco_coroutine*              coroutine = cco_coroutine_create(CCO_DEFAULT_STACK_SIZE, NULL);
+    std::vector<cco_coroutine*> coroutines;
+    int                         result = 0;
+
+    struct Args {
+        std::vector<cco_coroutine*>* coroutines;
+        int*                         result;
+    } args = {&coroutines, &result};
+
+    REQUIRE(coroutine != NULL);
+
+    cco_coroutine_start(
+        coroutine,
+        [](void* args_) {
+            cco_suspend();
+            Args* args = reinterpret_cast<Args*>(args_);
+            cco_await_with(
+                cco_await_not_ready,
+                [](cco_coroutine*, void* arg) {
+                    std::vector<cco_coroutine*>* coroutines = reinterpret_cast<std::vector<cco_coroutine*>*>(arg);
+                    coroutines->push_back(cco_this_coroutine());
+                    return true;
+                },
+                args->coroutines
+            );
+            *reinterpret_cast<int*>(args->result) = 1;
+        },
+        &args
+    );
+    REQUIRE(result == 0);
+    cco_resume(coroutine);
+
+    // Suppose that the coroutine is scheduled for execution after 3 seconds in a scheduler
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    auto c = coroutines.back();
+    coroutines.pop_back();
+    cco_resume(c);
+
+    REQUIRE(result == 1);
     cco_coroutine_destroy(coroutine);
 }
